67380e2616fd58e2c35b8f80cddb051873db0958
le duong hien thong <michaeldiavolos@gmail.com>
2022-11-03 16:59:38 -0400

    added initial commit and user authorization

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Authorization from './components/Authorization';
+
+function App() {
+  return (
+    <div className="App">
+      <Authorization />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Authorization.js
@@ -0,0 +1,27 @@
+
+
+export default function Authorization() {
+
+  const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+  const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
+  const REDIRECT_URI_AFTER_LOGIN = process.env.REACT_APP_REDIRECT_URI_AFTER_LOGIN;
+  const SPACE_DELIMITER = "%20";
+  const SCOPES = ["user-read-currently-playing", "user-read-playback-state"];
+  const SCOPES_URL_PARAM = SCOPES.join(SPACE_DELIMITER);
+
+
+
+  const handleLogin = () => {
+    window.location = `${SPOTIFY_AUTHORIZE_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI_AFTER_LOGIN}&scope=${SCOPES_URL_PARAM}&response_type=token&show_dialog=true`
+  }
+
+
+  return (
+    <div>
+      <div id="login">
+        <h1>First, log in to spotify</h1>
+        <button onClick={() => {handleLogin()}}>Log in</button>
+      </div>
+    </div>
+  )
+}
