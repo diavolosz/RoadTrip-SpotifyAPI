f5b07c0e4963eff5efd991e271051deacd6cb186
le duong hien thong <michaeldiavolos@gmail.com>
2022-11-04 02:02:44 -0400

    added input fied to enter client id

--- client/src/components/Authorization.js
+++ client/src/components/Authorization.js
@@ -4,13 +4,15 @@ import { useEffect, useState } from "react";
 export default function Authorization(props) {
 
   const { token, spotifyApi } = props
- 
-  const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+
+  const [CLIENT_ID, setCLIENT_ID] = useState("")
+
+  // const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
   const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
   const REDIRECT_URI_AFTER_LOGIN = process.env.REACT_APP_REDIRECT_URI_AFTER_LOGIN;
   const SPACE_DELIMITER = "%20";
   const SCOPES = [
-    "user-read-currently-playing", 
+    "user-read-currently-playing",
     "user-read-playback-state",
     "playlist-modify-private",
     "user-modify-playback-state"
@@ -19,11 +21,13 @@ export default function Authorization(props) {
 
   const [logged, setLogged] = useState(false)
 
-  const handleLogin = () => {
+  const handleLogin = (e) => {
+    e.preventDefault()
     window.location = `${SPOTIFY_AUTHORIZE_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI_AFTER_LOGIN}&scope=${SCOPES_URL_PARAM}&response_type=token&show_dialog=true`
+    console.log(e)
   }
 
-  const handleLogout = () => {
+  const handleLogout = (e) => {
     localStorage.clear()
     window.location = `${process.env.REACT_APP_REDIRECT_URI_AFTER_LOGIN}`
     setLogged(false)
@@ -43,7 +47,12 @@ export default function Authorization(props) {
           ? <h1>First, log in to spotify</h1>
           : <h1>Welcome ! You are logged in !</h1>
         }
-        <button onClick={() => { handleLogin() }}>Log in</button>
+        <form onSubmit={(e) => {handleLogin(e)}}>
+          <input value={CLIENT_ID} onChange={e => setCLIENT_ID(e.target.value)}/>
+          {/* <button onClick={() => { handleLogin() }}>Log in</button> */}
+          <button type="submit">Log in</button>
+
+        </form>
       </div>
       <div>
         <button onClick={() => { handleLogout() }}>Logout</button>
