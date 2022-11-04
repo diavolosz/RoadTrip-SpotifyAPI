3ea4fbde6348902e2de0bd01eb987621007f1c81
le duong hien thong <michaeldiavolos@gmail.com>
2022-11-04 15:27:10 -0400

    allow users to leave their session:

--- client/src/App.js
+++ client/src/App.js
@@ -76,6 +76,7 @@ function App() {
         token={invitationToken ? invitationToken : tokenInfo}
         invitationToken={invitationToken}
         setInvitationToken={setInvitationToken}
+        spotifyApi={spotifyApi}
       />
 
     </div>
--- client/src/components/JoinSession.js
+++ client/src/components/JoinSession.js
@@ -6,21 +6,29 @@ import { useState } from 'react'
 
 export default function JoinSession(props) {
 
-  const {token, invitationToken, setInvitationToken} = props
+  const {spotifyApi, token, invitationToken, setInvitationToken} = props
 
   const [ invitationTokenEntry, setInvitationTokenEntry ] = useState("")
 
   
-  const handleSessionJoin = (newTokenInvite) => {
-    console.log(newTokenInvite)
+  const handleSessionJoin = (e, newTokenInvite) => {
+    e.preventDefault()
+    setInvitationTokenEntry("")
+
     setInvitationToken(newTokenInvite)
+    spotifyApi.setAccessToken(localStorage.invitation_token)
     localStorage.setItem("invitation_token", newTokenInvite)
+  }
+
+  const handleSessionLeave = () => {
+    spotifyApi.setAccessToken(localStorage.access_token)
+    localStorage.removeItem("invitation_token")
   }
 
   return (
     <section>
       <h1>Enter new token here</h1>
-      <form onSubmit={() => handleSessionJoin(invitationTokenEntry)}>
+      <form onSubmit={(e) => handleSessionJoin(e, invitationTokenEntry)}>
         <input 
         value={invitationTokenEntry} 
         onChange={(e) => setInvitationTokenEntry(e.target.value)}
@@ -28,6 +36,7 @@ export default function JoinSession(props) {
         placeholder="enter token here"></input>
         <button type='submit'>click to join session</button>
       </form>
+        <button onClick={() => handleSessionLeave()}>Click to leave lession</button>
     </section>
   )
 }
