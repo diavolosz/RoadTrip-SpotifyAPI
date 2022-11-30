cf1159267a4183a792a1e77bda3abbcdc85d225b
le duong hien thong <michaeldiavolos@gmail.com>
2022-11-30 18:26:01 -0500

    depoy

--- client/src/components/Authorization.js
+++ client/src/components/Authorization.js
@@ -10,8 +10,9 @@ export default function Authorization(props) {
   const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
 
   // -----------------------------
-  const REDIRECT_URI_AFTER_LOGIN = process.env.REACT_APP_REDIRECT_URI_AFTER_LOGIN;
-  //  const REDIRECT_URI_AFTER_LOGIN = "https://iridescent-mousse-e23e3b.netlify.app/"; //use this for build deployment
+  // const REDIRECT_URI_AFTER_LOGIN = process.env.REACT_APP_REDIRECT_URI_AFTER_LOGIN;
+   const REDIRECT_URI_AFTER_LOGIN = "https://iridescent-mousse-e23e3b.netlify.app/"; 
+  //use this for build deployment
   // -----------------------------
 
   const SPACE_DELIMITER = "%20";
@@ -70,8 +71,9 @@ export default function Authorization(props) {
     localStorage.clear()
 
     // -----------------------------
-    window.location = `${process.env.REACT_APP_REDIRECT_URI_AFTER_LOGIN}`
-    //  window.location = REDIRECT_URI_AFTER_LOGIN //use this for build deployment
+    // window.location = `${process.env.REACT_APP_REDIRECT_URI_AFTER_LOGIN}`
+     window.location = REDIRECT_URI_AFTER_LOGIN 
+    //use this for build deployment
     //------------------------------
 
     setLogged(false)
